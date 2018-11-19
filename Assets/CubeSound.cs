65f07c83b1a2396cac0298ff54be0d501f4e2638
Honjo Kasumim <hnjksm555@gmail.com>
2018-11-19 17:41:20 +0900

    GoForwardUnityChan　課題
    課題まで実装しました

---
+++ Assets/CubeSound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSound : MonoBehaviour {
+
+    private AudioSource audioSource;
+
+    public AudioClip sound;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        audioSource.clip = sound;
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "UnityChan2D") {
+            audioSource.Play();
+        }
+    }
+}
